64cfcdcb82b67a0d462b02d708a4347a068bad72
Sven Meyer <git_dev@svenmeyer.com>
2021-01-27 04:26:50 +1100

    fix: added '0x' to ALARM_JOB_ID

--- ethereum/post_contracts_fund_migrations/post_contracts_fund_migration.js
+++ ethereum/post_contracts_fund_migrations/post_contracts_fund_migration.js
@@ -1,11 +1,19 @@
-const randomness = artifacts.require("Randomness");
+const randomness = artifacts.require("RandomNumberConsumer");
 const lottery = artifacts.require("Lottery");
 const governance = artifacts.require("Governance");
 
 module.exports = async function(deployer, network, accounts) {
-  var governanceContract = await governance.deployed();
-  var lotteryContract = await lottery.deployed();
-  var randomnessContract = await randomness.deployed();
 
+  var governanceContract = await governance.deployed();
+  console.log('governanceContract fetched :', governanceContract.address);
+
+  var lotteryContract = await lottery.deployed();
+  console.log('lotteryContract fetched    :', lotteryContract.address);
+
+  var randomnessContract = await randomness.deployed();
+  console.log('randomnessContract fetched :', randomnessContract.address);
+
+  console.log("Starting lottery ...");
   await lotteryContract.start_new_lottery(90);
+  console.log("Done");
 };
